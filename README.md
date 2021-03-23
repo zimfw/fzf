ad2a63aa3dd769e25a7e91e9a181de8cc65e347d
Eric Nielsen <eric@amalgamar.com.br>
2021-03-22 20:52:32 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+fzf
+===
+
+Configures [fzf] to use [fd] or [ripgrep] for faster fetching of files and
+directories names.
+
+Requirements
+------------
+
+[fzf] must be installed already. And either [fd] or [ripgrep] must be installed
+already too.
+
+[fzf]: https://github.com/junegunn/fzf
+[fd]: https://github.com/sharkdp/fd
+[ripgrep]: https://github.com/BurntSushi/ripgrep
---
+++ init.zsh
@@ -0,0 +1,18 @@
+if (( ${+commands[fd]} )); then
+  export FZF_DEFAULT_COMMAND='command fd -c always -H --no-ignore-vcs -E .git -tf'
+  export FZF_ALT_C_COMMAND='command fd -c always -H --no-ignore-vcs -E .git -td'
+  _fzf_compgen_path() {
+    command fd -c always -H --no-ignore-vcs -E .git -tf "${1}"
+  }
+  _fzf_compgen_dir() {
+    command fd -c always -H --no-ignore-vcs -E .git -td "${1}"
+  }
+  export FZF_DEFAULT_OPTS="--ansi ${FZF_DEFAULT_OPTS}"
+elif (( ${+commands[rg]} )); then
+  export FZF_DEFAULT_COMMAND="command rg -uu -g '!.git' --files"
+  _fzf_compgen_path() {
+    command rg -uu -g '!.git' --files "${1}"
+  }
+fi
+
+if (( ${+FZF_DEFAULT_COMMAND} )) export FZF_CTRL_T_COMMAND=${FZF_DEFAULT_COMMAND}
